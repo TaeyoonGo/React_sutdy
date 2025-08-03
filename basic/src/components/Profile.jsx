92c54d9ed162bcab92a55dae2dcb3b451c459f07
TaeyoonGo <taeyoongo007@gmail.com>
2025-08-03 20:33:53 +0900

    feat : 아바타 컴포넌트 만들기 작업 완료

---
+++ basic/src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+export default function Profile({image,name,title,isNew}) {
+    return (
+        <div className="profile">
+            <img className="photo"
+                 src={image}
+                 alt="avator"></img>
+            {isNew && <span className="new">new</span>}
+            <h1>{name}</h1>
+            <p>{title}</p>
+        </div>
+    );
+}
+
