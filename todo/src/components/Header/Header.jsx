1adcada88b66d4394f4b6a3cc7716117ead8721c
TaeyoonGo <taeyoongo007@gmail.com>
2025-08-18 17:58:26 +0900

    feat : 리엑트 TodoList 스터디 완

--- todo/src/components/Header/Header.jsx
+++ todo/src/components/Header/Header.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import styles from './Header.module.css';
+import {useDarkMode} from "../../context/DarkModeContext.jsx";
+import {HiMoon, HiSun} from "react-icons/hi";
 
-export default function Header() {
+export default function Header({filters, filter, onFilterChange}) {
+    const {darkMode, toggleDarkMode} = useDarkMode();
     return (
-       <Header>
-           {
-
-           }
-       </Header>
-    );
+        <header className={styles.header}>
+            <button onClick={toggleDarkMode} className={styles.toggle}>
+                {!darkMode && <HiMoon/>}
+                {darkMode && <HiSun/>}
+            </button>
+            <ul className={styles.filters}>
+                {
+                    filters.map((value, index) => <li key={index}>
+                        <button className={`${styles.filter} ${
+                            filter === value && styles.selected
+                        }`} onClick={() => onFilterChange(value)}>{value}</button>
+                    </li>)
+                }</ul>
+        </header>
+    )
 }
--- todo/src/components/Todo/Todo.jsx
+++ todo/src/components/Todo/Todo.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {FaTrashAlt} from "react-icons/fa";
+import styles from './Todo.module.css';
 
 export default function Todo({todo,onUpdate,onDelete}) {
-    const {text,status} = todo;
+    const {text,status,id} = todo;
     const handleChange = (e) => {
         const status = e.target.checked ? 'completed' : 'active'
         onUpdate({...todo,status})
@@ -11,13 +12,13 @@ export default function Todo({todo,onUpdate,onDelete}) {
        onDelete(todo)
     }
     return(
-        <li>
-            <input type="checkbox" id="checkbox"
+        <li className={styles.todo} >
+            <input className={styles.checkbox} type="checkbox" id={id}
                    checked={status === 'completed'}
                    onChange={handleChange}
                 />
-            <label htmlFor="checkbox">{text}</label>
-            <button onClick={handleDelete}><FaTrashAlt/></button>
+            <label htmlFor={id} className={styles.text}>{text}</label>
+            <span className={styles.icon}><button className={styles.button} onClick={handleDelete}><FaTrashAlt/></button></span>
         </li>
     )
 }
